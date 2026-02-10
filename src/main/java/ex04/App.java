31343a572204c9d074e33895de321eb78179adbb
KJcording <rmfdnjf98@gmil.com>
2026-02-10 15:39:59 +0900

    디자인패턴(전략,프록시,어댑터,싱글톤,템플릿메서드)

---
+++ src/main/java/ex04/App.java
@@ -0,0 +1,13 @@
+package ex01;
+// SOLID : 객체지향원칙 (D, O)
+// DIP : 추상적인 것에 의존하라
+// OCP : 새로운 코드에는 OPEN, 기존 코드에는 CLOSE
+public class App {
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+        Mouse mouse = new Mouse();
+        Doorman doorman = new Doorman();
+        doorman.쫓아내(mouse);
+
+    }
+}
---
+++ src/main/java/ex04/Doorman.java
@@ -0,0 +1,8 @@
+package ex01;
+
+public class Doorman {
+    // 책임
+    public void 쫓아내(Animal a){
+        System.out.println(a.getName() + "저리가");
+    }
+}
